7a1cf623620f48a997d28e2b7dbef2e151c727a7
RGB93 <rgblanksman@gmail.com>
2023-08-04 22:20:53 -0300

    task1

---
+++ src/main/java/com/ap/homebanking/models/Client.java
@@ -0,0 +1,47 @@
+package com.ap.homebanking.models;
+
+import org.hibernate.annotations.GenericGenerator;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Client {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO, generator = "native")
+    @GenericGenerator(name = "native", strategy = "native")
+    private Long id;
+    private String fistName;
+    private String lastName;
+
+    public Client(){}
+
+    public Client ( String firstName, String lastName){
+        this.fistName = firstName;
+        this.lastName = lastName;
+
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getFistName() {
+        return fistName;
+    }
+
+    public void setFistName(String fistName) {
+        this.fistName = fistName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+}

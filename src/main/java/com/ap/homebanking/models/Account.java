1a274a7ac002e710fd7f3ed0dd86365da248d144
RGB93 <rgblanksman@gmail.com>
2023-08-08 22:04:43 -0300

    task2

---
+++ src/main/java/com/ap/homebanking/models/Account.java
@@ -0,0 +1,67 @@
+package com.ap.homebanking.models;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import org.hibernate.annotations.GenericGenerator;
+
+import javax.persistence.*;
+import java.time.LocalDate;
+
+@Entity
+public class Account {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO, generator = "native")
+    @GenericGenerator(name = "native", strategy = "native")
+    private Long id;
+    private String number;
+    private LocalDate creationDate;
+    private Double balance;
+
+    @ManyToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name ="holder_id")
+    private Client holder;
+    public Account (){
+
+    }
+
+    public Account (String number, LocalDate creationDate, double balance){
+        this.number = number;
+        this.creationDate = creationDate;
+        this.balance = balance;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+
+    public String getNumber() {
+        return number;
+    }
+
+    public void setNumber(String number) {
+        this.number = number;
+    }
+
+    public LocalDate getCreationDate() {
+        return creationDate;
+    }
+
+    public void setCreationDate(LocalDate creationDate) {
+        this.creationDate = creationDate;
+    }
+
+    public Double getBalance() {
+        return balance;
+    }
+
+    public void setBalance(Double balance) {
+        this.balance = balance;
+    }
+
+    @JsonIgnore
+    public Client getHolder(){return holder;}
+
+    public void setHolder(Client holder) {
+        this.holder = holder;
+    }
+}
--- src/main/java/com/ap/homebanking/models/Client.java
+++ src/main/java/com/ap/homebanking/models/Client.java
@@ -2,10 +2,9 @@ package com.ap.homebanking.models;
 
 import org.hibernate.annotations.GenericGenerator;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
+import java.util.HashSet;
+import java.util.Set;
 
 @Entity
 public class Client {
@@ -14,29 +13,32 @@ public class Client {
     @GeneratedValue(strategy = GenerationType.AUTO, generator = "native")
     @GenericGenerator(name = "native", strategy = "native")
     private Long id;
-    private String fistName;
+    private String firstName ;
     private String lastName;
+    private String email;
+
+    @OneToMany(mappedBy = "holder", fetch = FetchType.EAGER)
+    Set<Account> accounts = new HashSet<>();
 
     public Client(){}
 
-    public Client ( String firstName, String lastName){
-        this.fistName = firstName;
+    public Client ( String firstName, String lastName, String email){
+        this.firstName = firstName;
         this.lastName = lastName;
-
+        this.email = email;
     }
 
     public Long getId() {
         return id;
     }
 
-    public String getFistName() {
-        return fistName;
+    public String getFirstName() {
+        return firstName;
     }
 
-    public void setFistName(String fistName) {
-        this.fistName = fistName;
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
     }
-
     public String getLastName() {
         return lastName;
     }
@@ -44,4 +46,22 @@ public class Client {
     public void setLastName(String lastName) {
         this.lastName = lastName;
     }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Set<Account> getAccounts() {
+        return accounts;
+    }
+
+    public void addAccount(Account account){
+        account.setHolder(this);
+        accounts.add(account);
+    }
 }
+
